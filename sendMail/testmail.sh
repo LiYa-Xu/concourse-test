25f4ddb5ea7fb2ff0e9ec8cf8fa9beb03477e523
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-02-20 15:42:33 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-ENV DEBIAN_FRONTEND noninteractive
 apt-get update
 apt-get -y install debconf-utils
 $debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 $debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-apt-get -y install mailutils
+RUN DEBIAN_FRONTEND=noninteractive apt-get -y install mailutils
 echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
