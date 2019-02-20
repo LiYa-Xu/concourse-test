12b45160d1d0f305a7433fcdddd11458f6bf2558
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-02-20 16:25:58 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -2,9 +2,7 @@
 #/usr/bin/debconf-set-selections
 apt-get update
 apt-get -y install debconf-utils
+cat ~/Dockerfile 
 echo "test"
-$debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
-$debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-RUN ENV DEBIAN_FRONTEND=noninteractive
-apt-get -y install mailutils
-echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
+ENV DEBIAN_FRONTEND=noninteractive
+echo "test111111"
