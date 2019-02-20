e47f8c4bbc264e43ee04993c20aa4ad787513b04
xuliya <xuliya@xuliyas-MacBook-Pro.local>
2019-02-20 21:23:17 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
+#/usr/bin/debconf-set-selections
 apt-get update
-apt-get -y upgrade
-apt-get -y install sendmail  
-apt-get -y install sendmail-cf
-apt-get -y install mailutils  
+apt-get -y install debconf-utils
+echo "debconf1111" $debconf
+DEBIAN_FRONTEND=noninteractive apt-get -y install mailutils
 echo "test1111111111"
-ps aux |grep sendmail
-
+mail -h
 echo "test55555555555"
-#echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
+echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
