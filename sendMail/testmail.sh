61af6647f909676c955140a0cebcb7c3c9e55879
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-02-20 11:03:34 +0800

    update testmail.sh

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+apt-get install mailutils
 echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
