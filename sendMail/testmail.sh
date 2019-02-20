282b80b1efb6ce97740c83d701cdd9ef60d0a86a
xuliya <xuliya@xuliyas-MacBook-Pro.local>
2019-02-20 20:44:49 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -9,7 +9,7 @@ $debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 $debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 DEBIAN_FRONTEND=noninteractive apt-get -y install mailutils
 echo "test1111111111"
-mail -h
+mail 
 
 echo "test55555555555"
 echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
