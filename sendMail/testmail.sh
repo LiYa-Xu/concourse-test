c094af9d935c6c781cc399b327e0a8242ceb4ac2
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-02-20 10:54:20 +0800

    update testmail.sh

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
