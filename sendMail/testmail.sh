54b25dd88d0b43e2b89b70e701debc9246faafff
xuliya <xuliya@xuliyas-MacBook-Pro.local>
2019-02-20 21:10:04 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -5,7 +5,7 @@ apt-get -y install sendmail
 apt-get -y install sendmail-cf
 apt-get -y install mailutils  
 echo "test1111111111"
-mail 
+mail -h
 
 echo "test55555555555"
 echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya@cn.ibm.com
