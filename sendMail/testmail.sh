a423878b57fb187ede8d6ba6b51b5b4c7e46116b
xuliya <xuliya@xuliyas-MacBook-Pro.local>
2019-02-20 21:35:37 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #/usr/bin/debconf-set-selections
 apt-get update
-apt-get -y install debconf-utils
+DEBIAN_FRONTEND=noninteractive apt-get -y install debconf-utils
 echo "debconf1111" $debconf
 $debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 $debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get -y install mailutils
+#DEBIAN_FRONTEND=noninteractive apt-get -y install mailutils
 echo "test1111111111"
 mail -h
 echo "test55555555555"
