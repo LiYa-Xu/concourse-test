a70eaea623778ec7c036f9a4ebf95196164fdc12
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-02-21 11:45:40 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get -y install debconf-utils
+apt-get -y install vim
 debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get -y install mailutils
 ls /etc/postfix/main.cf
+sed -i "s/inet_interfaces = all/inet_interfaces = loopback-only/g" /etc/postfix/main.cf
 vim /etc/postfix/main.cf
 echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya_0116@126.com
