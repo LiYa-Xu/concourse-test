04739e8906b1b215ce4fc8eee6e3452380c8b02e
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-02-21 11:41:18 +0800

    update

--- sendMail/testmail.sh
+++ sendMail/testmail.sh
@@ -5,4 +5,5 @@ debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get -y install mailutils
 ls /etc/postfix/main.cf
+vim /etc/postfix/main.cf
 echo "Email Content:This is the content of mail. Welcome to ouyangpeng's blog : http://blog.csdn.net/ouyang_peng/" | mail -s "Email Subject : Hello from Linux Server by shell" xuliya_0116@126.com
